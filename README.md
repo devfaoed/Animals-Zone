6df66d4f35125a3414c10ec860ee8e998a465ba4
Adedokun Faith <faithadedokun2018@gmail.com>
2021-09-10 06:02:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Petzone
+ A blog site to read and post about animals from all over the world
---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+
+const multer = require("multer") 
+const ejs = require("ejs");
+const methodOverride = require("method-override");
+
+
+
+// models for the article
+const Blog = require("./model/blog");
+
+// router for the article
+const blogRouter = require("./router/blog");
+
+
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/petzone", {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.use(methodOverride("_method"));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.set("view engine", "ejs");
+
+
+app.use(blogRouter);
+
+app.listen(3030, function(){
+    console.log("Blog server started successfully on port 3030");
+})
